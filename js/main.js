c6ab78c6f1a5dc5e615bbbdb2f962be1d166c2fc
Kristina <yarevyakinakristina@gmail.com>
2023-02-28 14:00:26 +0700

    added menu-burger

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
 $(document).ready(function(){
+    /* плавная прокрутка меню */
     $('nav a[href^="#"]').click(function () {
         let target = $(this).attr('href');
         $('html, body').animate({
-            scrollTop: $(target).offfset().top //скролл сверху вниз
-    }, 500);
+            scrollTop: $(target).offset().top //скролл сверху вниз
+    }, 1000);
+    /* добавить/удалить класс active */
+    $('nav a[href^="#"]').parent().removeClass('active');
+    $(this).parent().addClass('active');
+    $('.menu_mobile .menu').toggle(500);
+    $('.menu__burger').toggleClass('close');
     return false;
+    });
+    /* мобильное меню */
+    $('.menu__burger').click(function(){
+        $('.menu_mobile .menu').toggle(500);
+        $(this).toggleClass('close');
     });
 });
